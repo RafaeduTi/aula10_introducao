f0d41e8182d724f29f94da4caa49f49f245f60be
RafaeduTi <rafaeledu2002@outlook.com>
2025-03-10 14:27:40 -0300

    versão base do código

---
+++ app/contato/page.js
@@ -0,0 +1,70 @@
+'use client'
+import { useState } from "react"
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
+export default function Contato(){
+    const[nome,alteraNome] = useState("")
+    const[mensagem,alteraMensagem] = useState("")
+
+
+    function enviaMensagem(e){
+       e.preventDefault()
+       
+       
+        if
+       (nome==""||mensagem=="" ){
+            console.log ("Por favor, prencha todos os campos...")
+       }else{
+                   console.log ("Mensagem enviada com sucesso!")
+       }
+
+    }
+    
+
+
+    return (
+    <div>
+            <h1>Página de contato</h1>
+            <p>Entre em contato conosco.</p>
+            <hr/>
+            <form onSubmit={(e)=> enviaMensagem(e)}>
+                    <label>
+                        Digite seu nome:
+                        <input onChange={(e)=> alteraNome(e.target.value)  }/>
+
+                        </label>
+            <br/>
+                <label>
+                    Digite sua mensagem:
+                    <input onChange={(e)=> alteraMensagem(e.target.value)  }/>
+                </label>
+            <br/>
+            <button>Enviar</button>            
+            </form>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
+    </div>
+
+
+    )
+
+
+
+
+
+}
---
+++ app/page.js
@@ -0,0 +1,95 @@
+'use client'
+
+
+import { useState } from "react";
+import "./home.css"
+
+
+
+
+
+export default function Home() {
+  
+  
+  const [carrinho, alteraCarrinho] = useState(0)
+  
+  function removeCarrinho(){
+    if(carrinho>0){
+      alteraCarrinho(carrinho-1)
+    }
+   }
+  
+  
+  
+  return (
+    <div>
+       <h1>Conradito Market</h1>
+       <p>O melhor supermercado do bairro</p>
+       <hr/>
+       <p className="carrinho">Itens no carrinho: {carrinho}</p>
+      <button onClick={()=>removeCarrinho()}>Remover Iten</button>
+      <button onClick={()=>alteraCarrinho(carrinho-carrinho)}>Limpar Carrinho</button>
+      
+      <hr/>
+      <h2>Produtos</h2>
+      <div className="produto">    
+      <img src="https://placehold.co/150"/>
+      <p>Cenoura</p>
+      <p>R$4,50 (kg)</p>
+      <button onClick={ ()=> alteraCarrinho(carrinho + 1) }>Comprar</button>
+      </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
